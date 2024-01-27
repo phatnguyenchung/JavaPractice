9a0b8100952c6b6f57fa8298734d0046beb7efa4
phatnguyen <nguyenchungphat2502@gmail.com>
2024-01-27 14:33:58 +0700

    refactor code

--- LoopDivision.java
+++ LoopDivision.java
@@ -7,9 +7,7 @@ public class LoopDivision {
         Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         for (int i = 0; i <= n; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-            }
+            System.out.print(i % 2 == 0 ? i + " " : "");
         }
     }
 }
