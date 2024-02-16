900f33770542a9dca25221e4904777122354e7f8
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-02-16 14:05:43 +0700

    fix npe

--- NPE.java
+++ NPE.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class NPE {
     public static void main(String[] args){
-        String _text = null ;
+        String _text;
+        //Fix NPE
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input:");
+        _text = scanner.next();
         System.out.println(_text);
         int length = _text.length();
         System.out.println(length);
