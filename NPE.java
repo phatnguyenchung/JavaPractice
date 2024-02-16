5ebe490c74dcd939bbd65b783d817098e5a6f0b9
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-02-16 14:11:13 +0700

    format code

--- NPE.java
+++ NPE.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class NPE {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String _text;
         //Fix NPE
         Scanner scanner = new Scanner(System.in);
