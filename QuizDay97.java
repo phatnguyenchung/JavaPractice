443b66c45d2b42268b4f18bb5a72fbdc4c913722
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-02-21 17:07:29 +0700

    quiz day 97

---
+++ QuizDay97.java
@@ -0,0 +1,12 @@
+public class QuizDay97 {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
