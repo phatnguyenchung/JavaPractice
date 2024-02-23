5fc1632334c8d530b3997698cdff46341d5e7e01
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-02-23 14:50:59 +0700

    refactor code

--- HashSetJava.java
+++ HashSetJava.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 
 public class HashSetJava {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Integer> list1 = new ArrayList<>();
         list1.add(1);
         list1.add(2);
@@ -18,9 +17,9 @@ public class HashSetJava {
         list1.add(6);
         list1.add(7);
         System.out.println(list1);
-        HashSet<Integer> hashSet = new HashSet<Integer>(list1);
-        List<Integer> newList = new ArrayList<Integer>(hashSet);
-        for(Integer ob : newList){
+        HashSet<Integer> hashSet = new HashSet<>(list1);
+        List<Integer> newList = new ArrayList<>(hashSet);
+        for (Integer ob : newList) {
             System.out.print(ob + " ");
         }
     }
