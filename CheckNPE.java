daea9aa9f2c8533388519c869ca739cf2826786b
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-03-06 15:41:45 +0700

    check NPE

---
+++ CheckNPE.java
@@ -0,0 +1,12 @@
+public class CheckNPE {
+    public static void main(String[] args) {
+
+        String platform = null;
+
+        if ("LinkedIn".equals(platform)) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+    }
+}
