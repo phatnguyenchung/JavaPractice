44e24e0b8ce5d7bf89c351e3549158375351a932
NGUYENCHUNGPHAT\NGUYEN CHUNG PHAT <nguyenchungphat2502@gmail.com>
2024-03-10 12:19:15 +0700

    challengeII

---
+++ ChallengeII.java
@@ -0,0 +1,8 @@
+public class ChallengeII {
+    public static void main(String[] args){
+        int number = -1;
+        number <<= 10;
+        int result = number >>> 10;
+        System.out.println(result);
+    }
+}
