01d9b3ebaaabbf7a10a659339b0942c84b19c3e3
NGUYENCHUNGPHAT\NGUYEN CHUNG PHAT <nguyenchungphat2502@gmail.com>
2024-03-11 10:04:23 +0700

    valid perfect square

--- ValidPerfectSquare.java
+++ ValidPerfectSquare.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ValidPerfectSquare {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input num to check perfect square:");
         int num = scanner.nextInt();
@@ -9,7 +9,7 @@ public class ValidPerfectSquare {
     }
 
     public static boolean isPerfectSquare(int num) {
-        double sqrt  =  Math.sqrt(num);
+        double sqrt = Math.sqrt(num);
         return ((sqrt - Math.floor(sqrt)) == 0);
     }
 }
