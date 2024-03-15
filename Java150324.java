d05647de5f42485a6ba10026c90791dfe4909f37
NGUYENCHUNGPHAT\NGUYEN CHUNG PHAT <nguyenchungphat2502@gmail.com>
2024-03-15 14:49:05 +0700

    update code

--- Java150324.java
+++ Java150324.java
@@ -1,6 +1,9 @@
 public class Java150324 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int result = -5 % 3;
         System.out.println(result);
+        int x = 5;
+        int y = 2;
+        System.out.println(x / y);
     }
 }
