5efa6a36c608fa4008677ab032ef5b747218f16a
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-09-03 12:50:24 +0700

    update code

--- Quiz220224.java
+++ Quiz220224.java
@@ -1,7 +1,7 @@
 public class Quiz220224 {
     public static void main(String[] args) {
         int x = 5, y = 7;
-        boolean result = (x > y) && (x++ < y--);
+        boolean result = false;
         System.out.println(x);
         System.out.println(y);
         System.out.println(result);
