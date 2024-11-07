b7454ad7cfc86d36da7f5d33110bcc3590ef06d3
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-11-07 17:53:24 +0700

    code out num

---
+++ Test071124.java
@@ -0,0 +1,9 @@
+public class Test071124 {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 0; i < 5; i++) {
+            num = num++;
+        }
+        System.out.println(num);
+    }
+}
