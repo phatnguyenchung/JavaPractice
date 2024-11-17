7974655906d45871cbdf01c2093595dcf419e1bf
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-11-17 20:02:50 +0700

    refactor code

--- MinimumMovesToEqualArrayElementsII.java
+++ MinimumMovesToEqualArrayElementsII.java
@@ -4,16 +4,11 @@ public class MinimumMovesToEqualArrayElementsII {
     public int minMoves2(int[] nums) {
         int n = nums.length;
         int ans = 0;
-
         Arrays.sort(nums);
-
-        int identity = nums[n % 2 == 0 ? (n/2)-1 : n/2];
-
-        for(int e: nums) {
-            if(e == identity) continue;
-            else ans += Math.abs(e - identity);
+        int median = nums[n / 2]; // Use the median (middle element after sorting)
+        for (int num : nums) {
+            ans += Math.abs(num - median); // Sum of absolute differences
         }
-
         return ans;
     }
 
