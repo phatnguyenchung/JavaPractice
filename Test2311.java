2c281da8ca50421d5032270bf81e1ea272973a28
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2024-11-24 15:21:16 +0700

    clean code

--- Test2311.java
+++ Test2311.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Test2311 {
     public static void main(String[] args) {
-        int n;
         Scanner scanner = new Scanner(System.in);
+        int n;
         do {
-            System.out.println("Input n:");
-        } while ((n = scanner.nextInt()) < 0);
+            System.out.print("Input n: ");
+            n = scanner.nextInt();
+        } while (n < 0);
         System.out.println("Output:");
         for (int i = 0; i <= n; i++) {
-            System.out.printf("%d: %s%n", i, (i % 2 == 0) ? "even" : "odd");
+            String parity = (i % 2 == 0) ? "even" : "odd";
+            System.out.printf("%d: %s%n", i, parity);
         }
         scanner.close();
     }
