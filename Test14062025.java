754addf47a2eaacfd93cd52c163af75e406788f4
Nguyen Chung Phat <120073107+phatnguyenchung@users.noreply.github.com>
2025-06-14 22:02:08 +0700

    test code

---
+++ Test14062025.java
@@ -0,0 +1,15 @@
+public class Test14062025 {
+    public static void main(String[] args) {
+        System.out.print("Hello, Good morning");
+        System.out.print("Tina");
+
+        int n = 7;
+        if (n >= 5 && n<= 10){
+            System.out.println("\nhi");
+        }
+
+        if(!(n <= 5 || n>= 10)){
+            System.out.println("hello");
+        }
+    }
+}
